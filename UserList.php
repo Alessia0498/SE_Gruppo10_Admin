47f94f0a96a876a4aa9678bf6ebe330daed98456
Alessia0498 <A.DIFRANCESCO5@studenti.unisa.it>
2020-11-26 14:09:03 +0100

    From Insert User to ViewSystemUsers

--- UserList.php
+++ UserList.php
@@ -29,35 +29,35 @@
     <div class="tablein">
       <?php
 
-      if (!isset($_POST['registered'])) {
 
-        $users = array(
-          array("username" => "ale984", "role" => "maintainer"),
-          array("username" => "marc58", "role" => "maintainer"),
-          array("username" => "tony145", "role" => "maintainer"),
-          array("username" => "ale984", "role" => "maintainer"),
-          array("username" => "ale984", "role" => "maintainer")
-        );
 
-        echo "<table class='table2' border='1'>";
+      $users = array(
+        array("username" => "ale984", "role" => "maintainer"),
+        array("username" => "marc58", "role" => "maintainer"),
+        array("username" => "tony145", "role" => "maintainer"),
+        array("username" => "ale984", "role" => "maintainer"),
+        array("username" => "ale984", "role" => "maintainer")
+      );
 
-        echo "<tr>
+      echo "<table class='table2' border='1'>";
+
+      echo "<tr>
       <th>Username</th>
       <th>Role</th>
       </tr>";
 
 
 
-        foreach ($users as $x => $user) {
+      foreach ($users as $x => $user) {
 
 
-          echo "<tr>
+        echo "<tr>
       <td width='35%' height='100%' align='center'><a href='ViewSystemUsers.php?username=" . $user['username'] . "&role=" . $user['role'] . "'>" . $user["username"] . "</a></td>
       <td width='35%' height='100%' align='center'>" . $user["role"] . "</td>  
     </tr>";
-        }
-        echo "</table>";
       }
+      echo "</table>";
+
 
       ?>
     </div>
@@ -70,67 +70,7 @@
 
 
 
-  if (isset($_POST['registered']) && $_GET['create']) {
 
-    session_start();
-
-    $_SESSION['username'] = $_POST['username'];
-    $_SESSION['password'] = $_POST['password'];
-    $_SESSION['repassword'] = $_POST['repassword'];
-    $_SESSION['role'] = $_POST['role'];
-
-
-
-
-
-    if ($_SESSION['password'] == $_SESSION['repassword']) {
-      $message = "Succefully entered user!";
-    } else {
-
-      die('<h3 style="text-align: center; color: red">Passwords don\'t match. Try again!</h3>');
-    }
-
-
-    if (isset($message)) {
-      echo '<h3 style="text-align: center; color: green">' . $message . '</h3>';
-    }
-
-
-
-
-
-
-    $_SESSION['user'] = array(
-      array("username" => "ale984", "role" => "maintainer"),
-      array("username" => "marc58", "role" => "maintainer"),
-      array("username" => "tony145", "role" => "maintainer"),
-      array("username" => "ale984", "role" => "maintainer"),
-      array("username" => "ale984", "role" => "maintainer")
-    );
-
-
-    $_SESSION['user1'] = array("username" => $_SESSION['username'], "role" => $_SESSION['role']);
-    array_push($_SESSION['user'], $_SESSION['user1']);
-
-    echo "<table class='table2' border='1'>";
-
-    echo "<tr>
-      <th>Username</th>
-      <th>Role</th>
-      </tr>";
-
-
-
-    foreach ($_SESSION['user'] as $key => $val) {
-
-
-      print_r($val['username']);
-      echo "<br>";
-      print_r($val['role']);
-      echo "<br>";
-    }
-    echo "</table>";
-  }
 
   generateFooter() ?>
 
--- ViewSystemUsers.php
+++ ViewSystemUsers.php
@@ -15,15 +15,15 @@
 
   require_once 'Library.php';
   generateHeader(); ?>
-   
-   <div class="tableFunctionsModify">
-  <div class="tableFunctionsFloater"></div>
-  <a href="ModifyUser.php?modify=yes"><img src="modify.png" style="height:50px" title="Modify user"></a>
+
+  <div class="tableFunctionsModify">
+    <div class="tableFunctionsFloater"></div>
+    <a href="ModifyUser.php?modify=yes"><img src="modify.png" style="height:50px" title="Modify user"></a>
   </div>
 
   <div class="tableFunctionsDelete">
-  <div class="tableFunctionsFloater"></div>
-  <a href="DeleteUser.php?delete=yes"><img src="delete.jpg" style="height:50px" title="Delete user"></a>
+    <div class="tableFunctionsFloater"></div>
+    <a href="DeleteUser.php?delete=yes"><img src="delete.jpg" style="height:50px" title="Delete user"></a>
   </div>
 
   <?php
@@ -38,6 +38,72 @@
 
     echo "<p>Role: " . $_GET['role'] . "</p>";
   }
+
+  if (isset($_POST['registered']) && $_GET['create']) {
+
+    session_start();
+
+    $_SESSION['username'] = $_POST['username'];
+    $_SESSION['password'] = $_POST['password'];
+    $_SESSION['repassword'] = $_POST['repassword'];
+    $_SESSION['role'] = $_POST['role'];
+
+
+
+
+
+    if ($_SESSION['password'] == $_SESSION['repassword']) {
+      $message = "Succefully entered user!";
+    } else {
+
+      die('<h3 style="text-align: center; color: red">Passwords don\'t match. Try again!</h3>');
+    }
+
+
+    if (isset($message)) {
+      echo '<h3 style="text-align: center; color: green">' . $message . '</h3>';
+    }
+
+
+
+
+
+
+    $_SESSION['user'] = array(
+      array("username" => "ale984", "role" => "maintainer"),
+      array("username" => "marc58", "role" => "maintainer"),
+      array("username" => "tony145", "role" => "maintainer"),
+      array("username" => "ale984", "role" => "maintainer"),
+      array("username" => "ale984", "role" => "maintainer")
+    );
+
+
+    $_SESSION['user1'] = array("username" => $_SESSION['username'], "role" => $_SESSION['role']);
+    array_push($_SESSION['user'], $_SESSION['user1']);
+
+    echo "<table class='table2' border='1'>";
+
+    echo "<tr>
+      <th>Username</th>
+      <th>Role</th>
+      </tr>";
+    echo "<tr>
+      <td width='35%' height='100%' align='center'><a href='ViewSystemUsers.php?username=" . $_SESSION['username'] . "&role=" . $_SESSION['role'] . "'>" . $_SESSION["username"] . "</a></td>
+      <td width='35%' height='100%' align='center'>" . $_SESSION["role"] . "</td>  
+    </tr>";
+  }
+  echo "</table>";
+
+  //foreach ($_SESSION['user'] as $key => $val) {
+
+
+  //print_r($_SESSION['username']);
+  //echo "<br>";
+  //print_r($_SESSION['role']);
+  //echo "<br>";
+  // }
+  //echo "</table>";
+  //}
 
 
   ?>
