9a608572a5dab30878f66cc34c40b26f2f438409
AlessandraBoccuzzi <a.boccuzzi@studenti.unisa.it>
2020-11-28 20:08:01 +0100

    Changes in the shape of the form

--- InsertUser.php
+++ InsertUser.php
@@ -29,23 +29,23 @@
 	<div class="form">
 		<form method="post" action="ViewSystemUsers.php?create=yes" id="form1" name="form1" enctype="multipart/form-data">
 
-			<p class="p1"><label for="username"> Username: <input type="text" required="required" name="username" id="username" placeholder="Enter a username" />
-				</label></p>
+			<label for="username"> Username: <input type="text" required="required" name="username" id="username" placeholder="Enter a username" />
+				</label>
 
-			<p class="p1"><label for="role"> Role: <select name="role" id="role" class="role">
+			<label for="role"> Role: <select name="role" id="role">
 						<option value="maintainer">Mantainer</option>
 						<option value="planner">Planner</option>
 						<option value="system administrator">System Administrator</option>
 
 					</select>
-				</label></p>
+				</label>
 
 
-			<p class="p1"><label for="password">Password: <input type="password" required="required" name="password" id="password" placeholder="Enter a  password" />
-				</label></p>
+			<label for="password">Password: <input type="password" required="required" name="password" id="password" placeholder="Enter a  password" />
+				</label>
 
-			<p class="p1"><label for="repassword"> Confirm password: <input type="password" required="required" name="repassword" id="repassword" placeholder="Confirm password" />
-				</label></p>
+			<label for="repassword"> Confirm password: <input type="password" required="required" name="repassword" id="repassword" placeholder="Confirm password" />
+				</label>
 
 
 			<br><button type="submit" class="button" name="registered" value="Insert a new user">Insert a new user</button>
