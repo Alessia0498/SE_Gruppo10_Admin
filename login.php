1e7198cd3f43833ed67c21cbe09c91d0b6a4b651
Alessia0498 <A.DIFRANCESCO5@studenti.unisa.it>
2020-12-12 15:11:58 +0100

    Changes in index and login and add style for login

--- login.php
+++ login.php
@@ -2,186 +2,12 @@
 <html>
 
 <head>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <style>
-        body {
-            font-family: Arial, Helvetica, sans-serif;
-        }
-
-        /* Full-width input fields */
-        input[type=text],
-        input[type=password] {
-            width: 100%;
-            padding: 12px 20px;
-            margin: 8px 0;
-            display: inline-block;
-            border: 1px solid #ccc;
-            box-sizing: border-box;
-        }
-
-        /* Set a style for all buttons */
-        button {
-            background-color: #4CAF50;
-            color: white;
-            padding: 14px 20px;
-            margin: 8px 0;
-            border: none;
-            cursor: pointer;
-            width: 100%;
-        }
-
-        button:hover {
-            opacity: 0.8;
-        }
-
-        /* Extra styles for the cancel button */
-        .cancelbtn {
-            width: auto;
-            padding: 10px 18px;
-            background-color: #f44336;
-        }
-
-        /* Center the image and position the close button */
-        .imgcontainer {
-            text-align: center;
-            margin: 24px 0 12px 0;
-            position: relative;
-        }
-
-        img.avatar {
-            width: 10%;
-            border-radius: 50%;
-        }
-
-        .container {
-            padding: 16px;
-        }
-
-        span.psw {
-            float: right;
-            padding-top: 16px;
-        }
-
-        /* The Modal (background) */
-        .modal {
-            display: none;
-            /* Hidden by default */
-            position: fixed;
-            /* Stay in place */
-            z-index: 1;
-            /* Sit on top */
-            left: 0;
-            top: 0;
-            width: 100%;
-            /* Full width */
-            height: 100%;
-            /* Full height */
-            overflow: auto;
-            /* Enable scroll if needed */
-            background-color: rgb(0, 0, 0);
-            /* Fallback color */
-            background-color: rgba(0, 0, 0, 0.4);
-            /* Black w/ opacity */
-            padding-top: 60px;
-        }
-
-        /* Modal Content/Box */
-        .modal-content {
-            background-color: #fefefe;
-            margin: 5% auto 15% auto;
-            /* 5% from the top, 15% from the bottom and centered */
-            border: 1px solid #888;
-            width: 80%;
-            /* Could be more or less, depending on screen size */
-        }
-
-        /* The Close Button (x) */
-        .close {
-            position: absolute;
-            right: 25px;
-            top: 0;
-            color: #000;
-            font-size: 35px;
-            font-weight: bold;
-        }
-
-        .close:hover,
-        .close:focus {
-            color: red;
-            cursor: pointer;
-        }
-
-        /* Add Zoom Animation */
-        .animate {
-            -webkit-animation: animatezoom 0.6s;
-            animation: animatezoom 0.6s
-        }
-
-        @-webkit-keyframes animatezoom {
-            from {
-                -webkit-transform: scale(0)
-            }
-
-            to {
-                -webkit-transform: scale(1)
-            }
-        }
-
-        @keyframes animatezoom {
-            from {
-                transform: scale(0)
-            }
-
-            to {
-                transform: scale(1)
-            }
-        }
-
-        /* Change styles for span and cancel button on extra small screens */
-        @media screen and (max-width: 300px) {
-            span.psw {
-                display: block;
-                float: none;
-            }
-
-            .cancelbtn {
-                width: 100%;
-            }
-        }
-    </style>
-</head>
-
-<body>
-
-    <h2>Welcome to the maintenance system! Log in to continue! </h2>
-
-    <button onclick="document.getElementById('id01').style.display='block'" style="width:auto;">Login</button>
-
-    <div id="id01" class="modal">
-
-        <form class="modal-content animate" action="/action_page.php" method="post">
-            <div class="imgcontainer">
-                <span onclick="document.getElementById('id01').style.display='none'" class="close" title="Close Modal">&times;</span>
-                <img src="./assets/avatar.jpg" alt="Avatar" class="avatar">
-            </div>
-
-            <div class="container">
-                <label for="uname"><b>Username</b></label>
-                <input type="text" placeholder="Enter Username" name="uname" required>
-
-                <label for="psw"><b>Password</b></label>
-                <input type="password" placeholder="Enter Password" name="psw" required>
-
-                <button type="submit">Login</button>
-
-            </div>
-
-            <div class="container" style="background-color:#f1f1f1">
-                <button type="button" onclick="document.getElementById('id01').style.display='none'" class="cancelbtn">Cancel</button>
-
-            </div>
-        </form>
-    </div>
+    <title>Login</title>
+    <meta name="author" content="gruppo 10" />
+    <link rel="stylesheet" type="text/css" href="style.css" />
+    <meta name="content" content="Login" />
+    <link rel="icon" href="assets/service.jpg" type="image/jpg" />
+    <meta charset="utf-8" />
 
     <script>
         // Get the modal
@@ -194,6 +20,87 @@
             }
         }
     </script>
+
+</head>
+
+<body>
+    <?php
+    session_start();
+    require_once './common/library.php';
+    include './services/api.service.php';
+    ?>
+
+    <h2 style="text-align:center;">Welcome to the maintenance system! Log in to continue! </h2>
+
+    <div style="text-align:center;">
+        <button onclick="document.getElementById('id01').style.display='block'" style="width:auto;">Login</button>
+    </div>
+
+    <div id="id01" class="modal">
+
+        <form class="modal-content animate" action="<?php echo $_SERVER['PHP_SELF'] ?>" method=" post">
+            <div class="imgcontainer">
+                <span onclick="document.getElementById('id01').style.display='none'" class="close" title="Close Modal">&times;</span>
+                <img src="./assets/avatar.jpg" alt="Avatar" class="avatar">
+            </div>
+
+            <div class="container">
+                <label for="username"><b>Username</b></label>
+                <input type="text" placeholder="Enter Username" name="username" id="username" required>
+
+                <label for="password"><b>Password</b></label>
+                <input type="password" placeholder="Enter Password" name="passsword" id="password" required>
+
+                <div style="text-align:center;">
+                    <button type="submit" id="login" name="login" value="yes">Login</button>
+                </div>
+
+            </div>
+
+            <div style="text-align:center;" style="background-color:#f1f1f1">
+                <button type="button" name="cancel" id="cancel" onclick="document.getElementById('id01').style.display='none'" class="cancelbtn">Cancel</button>
+
+            </div>
+        </form>
+    </div>
+
+
+
+    <?php
+    if (isset($_GET['login'])) {
+
+        $response = Api::get_user($_GET['username']);
+        $data = json_decode($response, true);
+
+
+        if (isset($data["message"])) {
+            echo "<h3 class='error'>" . $data['message'] . "</h3>";
+            exit;
+        }
+
+
+
+
+        switch ($response['role']) {
+            case 'maintainer':
+                go_to_page("./screens/list-users.screen.php");
+                break;
+            case 'planner':
+                go_to_page("../SE_Gruppo10_Planner/screens/list-maintenance-activity.screen.php");
+                break;
+            case 'admin':
+                go_to_page("./screens/list-users.screen.php");
+                break;
+        }
+
+
+        $message = "Incorrect username or password! Try again";
+        go_to_page("login.php?error=yes");
+
+        echo '<h3 style="text-align: center; color: red">' . $message . '</h3>';
+    }
+
+    ?>
 
 </body>
 
