72763860a9c2529b05834dc4411aa871efb92f57
marco741 <marco741147@gmail.com>
2020-12-17 08:46:13 +0100

    Fixed deploy scripts

--- deploy.bat
+++ deploy.bat
@@ -1,1 +1,1 @@
-ssh -t root@167.99.254.2 "su - arma -c 'cd /var/www/SE_Gruppo10_Admin && git fetch --all && git pull --force && git checkout develop'"
+ssh -t root@167.99.254.2 "su - arma -c 'cd /var/www/app/SE_Gruppo10_Admin && git fetch --all && git pull --force && git checkout develop'"
